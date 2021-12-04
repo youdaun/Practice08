e73f0fe764860153cd2b281ef53aab16a5c195de
youdaun <yjyou960@naver.com>
2021-12-05 02:57:31 +0900

    ex08완료

--- src/com/javaex/ex08/FriendApp.java
+++ src/com/javaex/ex08/FriendApp.java
@@ -10,11 +10,11 @@ public class FriendApp {
 		Friend[] friend = new Friend[3];
 		
 		System.out.println("친구를 3명 등록해 주세요");
+		
 		for(int i=0; i<friend.length; i++) {
 			String info = sc.nextLine();
 			String[] fArray = info.split(" ");
-			Friend f = new Friend(fArray[0], fArray[1], fArray[2]);
-			friend[i] = f;
+			friend[i] = new Friend(fArray[0], fArray[1], fArray[2]);
 		}
 		
 		for(int i=0; i<friend.length; i++) {
